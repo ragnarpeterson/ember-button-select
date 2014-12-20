502c3b7e8c7d3a35a666ef69f6a0b0bfbd7ab1b1
Jason Kriss <jasonkriss@gmail.com>
2014-12-19 16:32:03 -0800

    DRY up the triggering of actions on parent

--- addon/components/button-option.js
+++ addon/components/button-option.js
@@ -1,5 +1,11 @@
 import Em from 'ember';
 
+function triggerOn(eventName, actionName) {
+  return Em.on(eventName, function() {
+    this.get('parentView').send(actionName, this);
+  });
+}
+
 export default Em.Component.extend({
   tagName: 'button',
   type: 'button',
@@ -8,19 +14,12 @@ export default Em.Component.extend({
   attributeBindings: ['type'],
   label: null,
   value: null,
+
   isSelected: Em.computed('parentView.selected', function () {
     return this.get('parentView.selected') === this.get('value');
   }).readOnly(),
 
-  register: Em.on('didInsertElement', function() {
-    this.get('parentView').send('register', this);
-  }),
-
-  unregister: Em.on('willDestroyElement', function() {
-    this.get('parentView').send('unregister', this);
-  }),
-
-  select: Em.on('click', function() {
-    this.get('parentView').send('select', this);
-  })
+  register: triggerOn('didInsertElement', 'register'),
+  unregister: triggerOn('willDestroyElement', 'unregister'),
+  select: triggerOn('click', 'select')
 });
